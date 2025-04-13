74f1d82b295421911513d263e885973ebf946ad4
piz1va0nn <piz1va0n@gmail.com>
2025-04-13 02:45:47 -0400

    feat:retrive data from BST

--- src/xtore/instance/HomomorphicBSTStorage.pyx
+++ src/xtore/instance/HomomorphicBSTStorage.pyx
@@ -16,151 +16,141 @@ cdef i32 BST_NODE_OFFSET = 24
 cdef i32 BUFFER_SIZE = 1 << 13
 
 cdef class HomomorphicBSTStorage (BasicStorage):
-    def __init__(self, StreamIOHandler io, CollisionMode mode):
-        self.io = io
-        self.mode = mode
-        self.headerSize = BST_STORAGE_HEADER_SIZE
-        initBuffer(&self.headerStream, <char *> malloc(BST_STORAGE_HEADER_SIZE), BST_STORAGE_HEADER_SIZE)
-        initBuffer(&self.stream, <char *> malloc(BUFFER_SIZE), BUFFER_SIZE)
-        self.comparingNode = self.createNode()
+	def __init__(self, StreamIOHandler io, CollisionMode mode):
+		self.io = io
+		self.mode = mode
+		self.headerSize = BST_STORAGE_HEADER_SIZE
+		initBuffer(&self.headerStream, <char *> malloc(BST_STORAGE_HEADER_SIZE), BST_STORAGE_HEADER_SIZE)
+		initBuffer(&self.stream, <char *> malloc(BUFFER_SIZE), BUFFER_SIZE)
+		self.comparingNode = self.createNode()
 
-    def __dealloc__(self):
-        releaseBuffer(&self.headerStream)
-        releaseBuffer(&self.stream)
+	def __dealloc__(self):
+		releaseBuffer(&self.headerStream)
+		releaseBuffer(&self.stream)
 
-    cdef i64 create(self):
-        self.rootPosition = self.io.getTail()
-        self.rootNodePosition = -1
-        self.writeHeader()
-        return self.rootPosition
+	cdef i64 create(self):
+		self.rootPosition = self.io.getTail()
+		self.rootNodePosition = -1
+		self.writeHeader()
+		return self.rootPosition
 
-    cdef RecordNode createNode(self):
-        raise NotImplementedError
+	cdef RecordNode createNode(self):
+		raise NotImplementedError
 
-    cdef writeHeader(self):
-        self.headerStream.position = 0
-        self.writeHeaderBuffer(&self.headerStream)
-        self.io.seek(self.rootPosition)
-        self.io.write(&self.headerStream)
-        self.isCreated = True
+	cdef writeHeader(self):
+		self.headerStream.position = 0
+		self.writeHeaderBuffer(&self.headerStream)
+		self.io.seek(self.rootPosition)
+		self.io.write(&self.headerStream)
+		self.isCreated = True
 
-    cdef writeHeaderBuffer(self, Buffer *stream):
-        setBuffer(stream, MAGIC, MAGIC_LENGTH)
-        setBuffer(stream, <char *> &self.rootNodePosition, 8)
+	cdef writeHeaderBuffer(self, Buffer *stream):
+		setBuffer(stream, MAGIC, MAGIC_LENGTH)
+		setBuffer(stream, <char *> &self.rootNodePosition, 8)
 
-    cdef readHeader(self, i64 rootPosition):
-        self.rootPosition = rootPosition
-        self.io.seek(self.rootPosition)
-        self.io.read(&self.headerStream, self.headerSize)
-        self.readHeaderBuffer(&self.headerStream)
+	cdef readHeader(self, i64 rootPosition):
+		self.rootPosition = rootPosition
+		self.io.seek(self.rootPosition)
+		self.io.read(&self.headerStream, self.headerSize)
+		self.readHeaderBuffer(&self.headerStream)
 
-    cdef readHeaderBuffer(self, Buffer *stream):
-        cdef bint isMagic = memcmp(MAGIC, self.headerStream.buffer, MAGIC_LENGTH)
-        self.headerStream.position += MAGIC_LENGTH
-        if isMagic != 0:
-            raise ValueError('Wrong Magic for BinarySearchTreeStorage')
-        self.rootNodePosition = (<i64*> getBuffer(&self.headerStream, 8))[0]
-    
-    cdef RecordNode get(self, RecordNode reference, RecordNode result):
-        if self.rootNodePosition < 0: return None
-        cdef i64 position = self.rootNodePosition
-        cdef i64 nodePosition
-        cdef i64 left
-        cdef i64 right
-        cdef i32 compareResult
-        cdef RecordNode stored
+	cdef readHeaderBuffer(self, Buffer *stream):
+		cdef bint isMagic = memcmp(MAGIC, self.headerStream.buffer, MAGIC_LENGTH)
+		self.headerStream.position += MAGIC_LENGTH
+		if isMagic != 0:
+			raise ValueError('Wrong Magic for BinarySearchTreeStorage')
+		self.rootNodePosition = (<i64*> getBuffer(&self.headerStream, 8))[0]
+	
+	cdef RecordNode get(self, RecordNode reference, RecordNode result):
+		if self.rootNodePosition < 0: return None
+		cdef i64 position = self.rootNodePosition
+		cdef i64 nodePosition
+		cdef i64 left
+		cdef i64 right
+		cdef i32 compareResult
+		cdef RecordNode stored
 
-        while True:
-            self.io.seek(position)
-            self.io.read(&self.stream, BST_NODE_OFFSET)
-            nodePosition = (<i64*> getBuffer(&self.stream, 8))[0]
-            left = (<i64*> getBuffer(&self.stream, 8))[0]
-            right = (<i64*> getBuffer(&self.stream, 8))[0]
-            stored = self.readNodeKey(nodePosition, result)
-            compareResult = reference.compare(stored)
-            
-            if compareResult == 0:
-                if stored == reference:
-                    self.readNodeValue(stored)
-                    return stored
-                else:
-                    if right > 0:
-                        position = right
-                    else:
-                        break
-            else:
-                if left > 0:
-                    position = left
-                else:
-                    break
+		while True:
+			self.io.seek(position)
+			self.io.read(&self.stream, BST_NODE_OFFSET)
+			nodePosition = (<i64*> getBuffer(&self.stream, 8))[0]
+			left = (<i64*> getBuffer(&self.stream, 8))[0]
+			right = (<i64*> getBuffer(&self.stream, 8))[0]
+			stored = self.readNodeKey(nodePosition, result)
 
-    cdef set(self, RecordNode reference):
-        cdef i64 placeHolder = -1
+			compareResult = reference.compare(stored)
 
-        if self.rootNodePosition < 0:
-            self.appendNode(reference)
-            self.stream.position = 0
-            setBuffer(&self.stream, <char *> &reference.position, 8)
-            setBuffer(&self.stream, <char *> &placeHolder, 8)
-            setBuffer(&self.stream, <char *> &placeHolder, 8)
-            self.rootNodePosition = self.io.getTail()
-            self.io.append(&self.stream)
-            print("Created root node at position", self.rootNodePosition)
-            return
-        
-        cdef i64 position = self.rootNodePosition
-        cdef i64 nodePosition
-        cdef i64 left
-        cdef i64 right
-        cdef i32 compareResult
-        cdef RecordNode stored
-        while True:
-            print("\nCurrent node position:", position)
-            self.io.seek(position)
-            self.io.read(&self.stream, BST_NODE_OFFSET)
-            nodePosition = (<i64*> getBuffer(&self.stream, 8))[0]
-            left = (<i64*> getBuffer(&self.stream, 8))[0]
-            right = (<i64*> getBuffer(&self.stream, 8))[0]
-            print(f"Node at {position}: left={left}, right={right}")
-            stored = self.readNodeKey(nodePosition, self.comparingNode)
+			if compareResult == 1:
+				if left > 0:
+					position = left
+				else:
+					return stored
 
-            compareResult = reference.compare(stored)
+			else:
+				if right > 0:
+					position = right
+				else:
+					return stored
+			
 
-            if compareResult == 1:
-                if left > 0:
-                    position = left
-                    print("left position:", left)
-                else:
-                    self.appendNode(reference)
-                    self.stream.position = 0
-                    setBuffer(&self.stream, <char *> &reference.position, 8)
-                    setBuffer(&self.stream, <char *> &placeHolder, 8)
-                    setBuffer(&self.stream, <char *> &placeHolder, 8)
-                    left = self.io.getTail()
-                    self.io.append(&self.stream)
+	cdef set(self, RecordNode reference):
+		cdef i64 placeHolder = -1
 
-                    self.stream.position = 0
-                    setBuffer(&self.stream, <char *> &left, 8)
-                    self.io.seek(position + 8)
-                    self.io.write(&self.stream)
-                    print("appendNode left")
-                    break
-            else:
-                if right > 0:
-                    position = right
-                    print("right position", right)
-                else:
-                    self.appendNode(reference)
-                    self.stream.position = 0
-                    setBuffer(&self.stream, <char *> &reference.position, 8)
-                    setBuffer(&self.stream, <char *> &placeHolder, 8)
-                    setBuffer(&self.stream, <char *> &placeHolder, 8)
-                    right = self.io.getTail()
-                    self.io.append(&self.stream)
+		if self.rootNodePosition < 0:
+			self.appendNode(reference)
+			self.stream.position = 0
+			setBuffer(&self.stream, <char *> &reference.position, 8)
+			setBuffer(&self.stream, <char *> &placeHolder, 8)
+			setBuffer(&self.stream, <char *> &placeHolder, 8)
+			self.rootNodePosition = self.io.getTail()
+			self.io.append(&self.stream)
+			return
+		
+		cdef i64 position = self.rootNodePosition
+		cdef i64 nodePosition
+		cdef i64 left
+		cdef i64 right
+		cdef i32 compareResult
+		cdef RecordNode stored
+		while True:
+			self.io.seek(position)
+			self.io.read(&self.stream, BST_NODE_OFFSET)
+			nodePosition = (<i64*> getBuffer(&self.stream, 8))[0]
+			left = (<i64*> getBuffer(&self.stream, 8))[0]
+			right = (<i64*> getBuffer(&self.stream, 8))[0]
+			stored = self.readNodeKey(nodePosition, self.comparingNode)
 
-                    self.stream.position = 0
-                    setBuffer(&self.stream, <char *> &right, 8)
-                    self.io.seek(position + 16)
-                    self.io.write(&self.stream)
-                    print("appendNode right")
-                    break
+			compareResult = reference.compare(stored)
+
+			if compareResult == 1:
+				if left > 0:
+					position = left
+				else:
+					self.appendNode(reference)
+					self.stream.position = 0
+					setBuffer(&self.stream, <char *> &reference.position, 8)
+					setBuffer(&self.stream, <char *> &placeHolder, 8)
+					setBuffer(&self.stream, <char *> &placeHolder, 8)
+					left = self.io.getTail()
+					self.io.append(&self.stream)
+					self.stream.position = 0
+					setBuffer(&self.stream, <char *> &left, 8)
+					self.io.seek(position + 8)
+					self.io.write(&self.stream)
+					break
+			else:
+				if right > 0:
+					position = right
+				else:
+					self.appendNode(reference)
+					self.stream.position = 0
+					setBuffer(&self.stream, <char *> &reference.position, 8)
+					setBuffer(&self.stream, <char *> &placeHolder, 8)
+					setBuffer(&self.stream, <char *> &placeHolder, 8)
+					right = self.io.getTail()
+					self.io.append(&self.stream)
+					self.stream.position = 0
+					setBuffer(&self.stream, <char *> &right, 8)
+					self.io.seek(position + 16)
+					self.io.write(&self.stream)
+					break
