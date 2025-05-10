bc7d00c24f7e3bbc68be369c40f36f006651661e
Yotsaphon Siribannawit <yotsaphon77@gmail.com>
2025-05-10 23:45:30 +0700

    adjust parameter

--- src/xtore/service/ErrorHandler.pyx
+++ src/xtore/service/ErrorHandler.pyx
@@ -6,12 +6,6 @@ from xtore.protocol.RecordNodeProtocol cimport DatabaseOperation
 from xtore.algorithm.PrimeRing cimport PrimeRing
 import asyncio
 
-cdef object METHOD = {
-	DatabaseOperation.SET: "SET",
-	DatabaseOperation.GET: "GET",
-	DatabaseOperation.GETALL: "GETALL"
-}
-
 cdef class ErrorHandler:
 	def __init__(self):
 		self.failedData = []
@@ -23,18 +17,18 @@ cdef class ErrorHandler:
 				result = await tcpFunction(processID, message, host, port)
 				return result
 			except (asyncio.TimeoutError, ConnectionRefusedError, ConnectionError, OSError) as e:
-				print(f"[{processID}]({host}:{port}) >> ERROR {str(e)}")
+				#print(f"[{processID}]({host}:{port}) >> ERROR {str(e)}")
 				self.failedData.append((processID, message, host, port))
 				return (0, 0)
 
-	async def resend(self, object tcpFunction, DatabaseOperation method):
+	async def resend(self, object tcpFunction):
 		cdef list tasks = []
 		cdef People record = People()
 		cdef StorageUnit storageUnit
 		cdef PrimeNode primeRingNode
 		cdef dict nodes = {}
 		while self.failedData:
-			print(f">> Retrying {len(self.failedData)} failed request(s)...")
+			#print(f">> Retrying {len(self.failedData)} failed request(s)...")
 			current = self.failedData
 			self.failedData = []
 			for processID, message, host, port in current:
